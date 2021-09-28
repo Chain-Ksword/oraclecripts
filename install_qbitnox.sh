b7676d0463940f9b54515dc72169811f31ab7500
Ubuntu <chainksword@gmail.com>
2021-09-28 10:36:56 +0000

    points towards the deb files now

--- install_qbitnox.sh
+++ install_qbitnox.sh
@@ -5,10 +5,12 @@ select i in amd64 arm64
 do
 	case $i in
 		amd64) 
-			echo 'AMD64'
+			read -p 'Press enter to install qbittorrent-nox 4.1.7amd64'
+			sudo apt install ./packages/qbittorrent-nox_4.1.7-1ubuntu3_amd64.deb -y
 			break;;
 		arm64)
-			echo 'ARM64'
+			read -p 'Press enter to install qbittorrent-nox 4.1.7arm64'
+			sudo apt install ./packages/qbittorrent-nox_4.1.7-1ubuntu3_arm64.deb -y
 			break;;
 		*)
 			echo 'Inavlid option!'
