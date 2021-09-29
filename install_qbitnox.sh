e8c9f4976c8b0e32374e2248b2ddf16713fb4969
Ubuntu <chainksword@gmail.com>
2021-09-29 07:09:34 +0000

    Trying a dif approach

--- install_qbitnox.sh
+++ install_qbitnox.sh
@@ -5,8 +5,10 @@ select i in amd64 arm64
 do
 	case $i in
 		amd64) 
-			read -p 'Press enter to install qbittorrent-nox 4.3.6amd64'
-			sudo apt install ./packages/qbittorrent-nox_4.3.6.99~202107121021-7389-3ac8c97e6~ubuntu20.10.1_amd64.deb -y
+			read -p 'Press enter to install qbittorrent-nox 4.3.7x86_64'
+			mkdir -p ~/bin && source ~/.profile
+			mv x86_64-qbittorrent-nox-4.3.7 ~/bin/qbittorrent-nox
+			chmod 700 ~/bin/qbittorrent-nox
 			break;;
 		arm64)
 			read -p 'Press enter to install qbittorrent-nox 4.3.6arm64'
@@ -16,8 +18,8 @@ do
 			echo 'Inavlid option!'
 	esac
 done
-read -p 'Press enter to stop qbittorrent-nox from updating'
-sudo apt-mark hold qbittorrent-nox
+#read -p 'Press enter to stop qbittorrent-nox from updating'
+#sudo apt-mark hold qbittorrent-nox
 echo -e 'Add preferences to: ~/.config/qBittorrent/\nand .torrent files, logs, etc to: ~/.local/share/data/qBittorrent/'
 echo 'Make sure you have torrent data in the correct folders'
 echo '************************************************************'
