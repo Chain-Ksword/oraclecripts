a5380831afcc4d64c006af18de759502cb956583
Ubuntu <chainksword@gmail.com>
2021-09-29 04:54:15 +0000

    replaced with qbit4.3.6

--- install_qbitnox.sh
+++ install_qbitnox.sh
@@ -5,12 +5,12 @@ select i in amd64 arm64
 do
 	case $i in
 		amd64) 
-			read -p 'Press enter to install qbittorrent-nox 4.1.7amd64'
-			sudo apt install ./packages/qbittorrent-nox_4.1.7-1ubuntu3_amd64.deb -y
+			read -p 'Press enter to install qbittorrent-nox 4.3.6amd64'
+			sudo apt install ./packages/qbittorrent-nox_4.3.6.99~202107121021-7389-3ac8c97e6~ubuntu20.10.1_amd64.deb -y
 			break;;
 		arm64)
-			read -p 'Press enter to install qbittorrent-nox 4.1.7arm64'
-			sudo apt install ./packages/qbittorrent-nox_4.1.7-1ubuntu3_arm64.deb -y
+			read -p 'Press enter to install qbittorrent-nox 4.3.6arm64'
+			sudo apt install ./packages/qbittorrent-nox_4.3.6.99~202107121021-7389-3ac8c97e6~ubuntu20.10.1_arm64.deb -y
 			break;;
 		*)
 			echo 'Inavlid option!'
