66e7a7bf1308ae6ed24a4275d38583ba8f5dbf61
Ubuntu <chainksword@gmail.com>
2021-09-29 04:57:33 +0000

    typo

--- install_qbitnox.sh
+++ install_qbitnox.sh
@@ -1,4 +1,4 @@
-echo ************************************************************
+echo '************************************************************'
 read -p 'Press enter to continue with installing qbittorrent-nox'
 PS3='Select if the cpu is arm64 or amd64'
 select i in amd64 arm64
@@ -20,4 +20,4 @@ read -p 'Press enter to stop qbittorrent-nox from updating'
 sudo apt-mark hold qbittorrent-nox
 echo -e 'Add preferences to: ~/.config/qBittorrent/\nand .torrent files, logs, etc to: ~/.local/share/data/qBittorrent/'
 echo 'Make sure you have torrent data in the correct folders'
-echo ************************************************************
+echo '************************************************************'
