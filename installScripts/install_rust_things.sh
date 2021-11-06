f4042b1bcb19a3a451059dafe94b6643ea6ba3de
Ubuntu <chainksword@gmail.com>
2021-11-06 18:54:35 +0000

    added bandwhich

--- installScripts/install_rust_things.sh
+++ installScripts/install_rust_things.sh
@@ -5,6 +5,6 @@ NC='\033[0m' # No Color
 echo -e "************************************************************${GREEN}"
 echo -e "Installs dust exa prcs bandwhich and bat"
 read -p "Press enter to continue with installing cargo and rust programs"
-sudo apt update; sudo apt install cargo
+sudo apt update; sudo apt install cargo bandwhich
 cargo install dust exa procs bandwhich bat
 echo -e "${NC}************************************************************"
