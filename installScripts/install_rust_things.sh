6f3221cf217689650569d9c648a69daa426e83bc
Ubuntu <chainksword@gmail.com>
2021-11-13 05:55:24 +0000

    added grex and macchina

--- installScripts/install_rust_things.sh
+++ installScripts/install_rust_things.sh
@@ -3,8 +3,9 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 echo -e "************************************************************${GREEN}"
-echo -e "Installs dust exa prcs bandwhich and bat"
+echo -e "Installs dust exa prcs bandwhich bat macchina and grex"
 read -p "Press enter to continue with installing cargo and rust programs"
-sudo apt update; sudo apt install cargo bandwhich grex
-cargo install dust exa procs bandwhich bat
+sudo apt update; sudo apt install cargo
+cargo install du-dust exa procs bandwhich bat macchina grex
+echo -e "${RED}Install cargo-update dependencies before attempting to install it!"
 echo -e "${NC}************************************************************"
