659d9099d9935bae537a2ad5af2d28ba20dafce5
Ubuntu <chainksword@gmail.com>
2021-11-13 19:09:55 +0000

    added zoxide db

--- otherScripts/oraclebackup.sh
+++ otherScripts/oraclebackup.sh
@@ -5,6 +5,6 @@ sudo zip -rq \
 	/home/ubuntu/.config/{macchina,qBittorrent,rclone,starship.toml}\
 	/home/ubuntu/{.vim,.thelounge,.bashrc,.bash_aliases}\
 	/opt/wallabag/\
-	/home/ubuntu/.local/share/qBittorrent/\
+	/home/ubuntu/.local/share/{qBittorrent,zoxide}/\
 	/home/ubuntu/discord/Kurisu/config.json
 rclone copy oracleArmBak.zip drive: -Pv
